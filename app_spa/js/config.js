69b1a6347ea4a9cf9720fb5094b50d06aeb8dcf0
Steven Marmion <128723359+stevenMarmion@users.noreply.github.com>
2024-04-02 11:42:35 +0200

    Delete app_spa directory

--- app_spa/js/config.js
+++
@@ -1,1 +0,0 @@
-export const ENDPOINT = 'http://localhost:3000/articles';
--- app_spa/js/views/pages/Error404.js
+++
@@ -1,9 +0,0 @@
-// Instantiate API
-export default class Error404 {
-    async render () {
-        let view =  `
-            <h2>Error 404</h2>
-        `;
-        return view
-    }
-}
